04efa421c24b95a972f00dd5c3a15c802c202a78
Prichindel Mihai <mihai.prichindel@gmail.com>
2019-12-16 18:30:54 +0200

    Initial commit

---
+++ src/Class1.java
@@ -0,0 +1,24 @@
+public class Class1 {
+
+    public static void main(String[] args) {
+        System.out.println("Acesta este un alt mesaj");
+
+
+        boolean b = 3 < 5;
+
+        System.out.println(" 3<5 este " + b);
+
+        int variabilaMeaInt = 34;
+        int variabilaMeaInt2 = 45;
+
+        int summaVariabile = variabilaMeaInt + variabilaMeaInt2;
+        System.out.println(" suma " + summaVariabile);
+
+    }
+
+
+}
+
+
+
+
